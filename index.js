2bd194ab5b4af6c2a73d981b449ffb87b3828fb7
Your Name <you@example.com>
2021-01-20 12:20:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^0-9a-zA-Z '-]/g, '')
+  }
+  static titleize(string) {
+    const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return string.split(" ").map((word, i) => smallWords.includes(word) && i !== 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+  }
 }
